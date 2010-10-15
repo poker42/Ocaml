d77fb54a90d2b6d7770b482e656e10cbe2a2502c
spike31 <gilles.gauthier31@gmail.com>
2010-10-15 19:22:22 +0200

    Signed-off-by: spike31 <gilles.gauthier31@gmail.com>

--- Aetoile.ml
+++ Aetoile.ml
@@ -1,11 +1,9 @@
-ï»¿(** Aetoile.ml **)
-
 (**************************************************************************)	
 let etat_aux
 (**************************************************************************)
 (ft : ('a -> ('b * ('a * int)) list))   	  (** Fonction de transition **)
 (e : 'a)									  (** Etat ********************)
-(op : 'b)									  (** Opération ***************)
+(op : 'b)									  (** OpÃ©ration Ã  effectuer ***************)
  = let res = ft e in List.filter (fun (x,(l,v)) -> x = op) res;;
  
  
@@ -14,7 +12,7 @@ let etatSuivant
 (**************************************************************************)
 (ft : ('a -> ('b * ('a * int)) list))         (** Fonction de transition **)
 (e : 'a)   							          (** Etat ********************)
-(op : 'b) 							          (* Opération à effectuer ****)
+(op : 'b) 							          (* OpÃ©ration Ã  effectuer ****)
 : 'a
 (**************************************************************************)
 = let n = etat_aux ft e op in match n with
@@ -28,7 +26,7 @@ let coutOp
 (**************************************************************************)	
 (ft : ('a -> ('b * ('a * int)) list))  		  (** Fonction de transition **)
 (e : 'a)   							   		  (** Etat ********************)
-(op : 'b) 							   		  (* Opération à effectuer ****)
+(op : 'b) 							   		  (* OpÃ©ration Ã  effectuer ****)
 : int
 (**************************************************************************)
 = let n = etat_aux ft e op in match n with
@@ -42,9 +40,10 @@ let coutOp
 
 (**************************************************************************)
 let rec etatFinal
+(**************************************************************************)
 (ft : ('a -> ('b * ('a * int)) list))  		  (** Fonction de transition **)
 (e : 'a)   							   		  (** Etat ********************)
-(op : 'b list) 						   		  (* Opération à effectuer ****)
+(op : 'b list) 						   		  (* OpÃ©ration Ã  effectuer ****)
 : 'a
 (**************************************************************************)
 = match op with
@@ -56,9 +55,10 @@ let rec etatFinal
 
 (**************************************************************************)
 let rec coutParcouru
+(**************************************************************************)
 (ft : ('a -> ('b * ('a * int)) list))  		  (** Fonction de transition **)
 (e : 'a)   							   		  (** Etat ********************)
-(op : 'b list) 						   		  (* Opération à effectuer ****)
+(op : 'b list) 						   		  (* OpÃ©ration Ã  effectuer ****)
 : int
 (**************************************************************************)
 = match op with
@@ -76,16 +76,18 @@ type ('etatT, 'opT) chemin =
 	ops    : 'opT list;
 	final  : 'etatT;
 	cout   :  int;
-	estim  :  int };;
+	estim  :  int; };;
 
 	
 (**************************************************************************)
 let creerChemin
+(**************************************************************************)
 (ft : ('a -> ('b * ('a * int)) list))  		  (** Fonction de transition **)
 (h  : ('a -> int))
 (e  : 'a)
 (op : 'b list)
 :('a,'b) chemin
+(**************************************************************************)
 =
 {
 	depart = e;
@@ -94,5 +96,65 @@ let creerChemin
 	cout = coutParcouru ft e op;
 	estim = h (etatFinal ft e op);
 	};;
+		
+(** Exemple:
+	let ch1 = creerChemin g1.opPoss g1.hEtat "A" ["b";"h";"h"]
+	let ch2 = creerChemin g1.opPoss g1.hEtat "A" ["h";"b"]
+	let ch3 = creerChemin g1.opPoss g1.hEtat "A" ["b"];;
+	**)
+
+
+(**************************************************************************)	
+(* Fonction auxiliaire pour afficherChemin								  *)
+(**************************************************************************)
+let rec aff_aux ft d op cu ct = match op with
+[] -> print_string d; print_string " "; print_string "cout: ";print_int cu; print_string " estim: "; print_int ct; print_newline();
+| a::b -> let s = etatSuivant ft d a in begin 
+print_string d; print_string " "; print_string a; print_string " "; end; aff_aux ft s b cu ct;;
 	
-(** Exemple:	 let f = creerChemin g1.opPoss g1.hEtat "A" ["b";"h";"h"];; **)
+	
+(**************************************************************************)
+let afficherChemin
+(**************************************************************************)
+(fpe : 'a -> unit)
+(fpo : 'b -> unit)
+(ft  : ('a -> ('b * ('a * int)) list))
+(ch  : ('a,'b) chemin)
+(**************************************************************************)
+= aff_aux ft ch.depart ch.ops ch.cout ch.estim;; 
+
+(*	afficherChemin print_string print_string g1.opPoss ch1;; *)
+
+
+
+(**************************************************************************)
+let supprimerEtat
+(**************************************************************************)
+(e : 'a)
+(lch : ('a,'b) chemin list)
+: ('a,'b) chemin list
+(**************************************************************************)
+= List.filter (fun x -> x.final <> e) lch;;
+
+
+
+(**************************************************************************)
+let rec insererChemin
+(**************************************************************************)
+(ch1 : ('a,'b) chemin )
+(lch : ('a,'b) chemin list)
+: ('a,'b) chemin list
+(**************************************************************************)
+= 
+match lch with 
+[] -> [ch1]
+| a::r ->  if (ch1.cout + ch1.estim)<(a.cout + a.estim) 
+			then ch1::lch
+			else a::insererChemin ch1 r;;
+
+
+let ch4 = creerChemin g1.opPoss g1.hEtat "A" ["h";"h"];;
+let ch5 = creerChemin g1.opPoss g1.hEtat "A" ["b";"h"];;
+(*   insererChemin ch4 lch;;    *)
+
+
---
+++ tp1.ml
@@ -0,0 +1,121 @@
+(***********************************)
+let insererEnTete
+(***********************************)
+( x : 'a) (** un objet de type quelconque *)
+(l : 'a list) (* une liste d'éléments du meme type que x *)
+(*******************)
+(* retourne une liste formée en ajoutant x en tete de l *)
+: 'a list
+(*******************)
+= x::l;;
+
+
+(***********************************)
+let rec insererEnQueue 
+(***********************************)
+(x :'a) (* element de type quelconque *)
+(l : 'a list) (* une liste quelconque *)
+: 'a list (* retourne une liste de quelconque *)
+ =
+match l with
+[] -> x::[]  (** si vide on insere x dans la liste vide *)
+| h::s -> h::insererEnQueue x s;; (** si element on l'ajoute en tete *)
+
+(***********************************)
+let rec somme = function
+(***********************************)
+[] -> 0   (* liste vide on renvoie 0 *)
+| x::r -> x+somme r;;  (* on ajoute la valeur de la tete *)
+(* retourne un type int *)
+
+(***********************************)
+let maxi
+(***********************************)
+ (x : int)   (* premiere valeur *)
+ (y : int)   (* seconde valeur *)
+ : int
+ =
+ if x<y then y else x;;  (** renvoie la plus grande des deux *)
+
+(***********************************)
+let maximum
+(***********************************)
+ (l : int list) (* la liste *)
+ : int
+ =
+ List.fold_right maxi l 0;; (** applique la fonction max à la liste l**)
+
+
+(***********************************)
+let rec insererEntierDansListeOrdonnee
+(***********************************)
+(x : int) (* element à insérer *)
+(l : int list)
+: int list (* liste ordonnée *)
+ = match l with  (* on regarde l *)
+[] -> [x]  (* si liste vide on renvoie juste la liste avec x *)
+| h::r -> if x<h then x::l (* si x<h on a trouvé la position à insérer *)
+                 else h::insererEntierDansListeOrdonnee x r;; (* sinon on continu de chercher *)
+
+
+(*** Liste d'associations ***)
+let l = [2,'b';3,'c';4,'z';8,'p'];;
+
+(***********************************)
+(** MiniArec : renvoie le couple   *)
+(** dont la valeur est minimum    **)
+
+(** Entrée : 'a list              **)
+(** Sortie : 'a                   **)
+let minimumArec = function
+(***********************************)
+	|[] -> failwith "Erreur"
+	|t::q ->List.fold_left min t q
+;;
+
+(*************************************)
+(** minimumA : Renvoie l'objet associé
+à la valeur minimum du couple        *)
+(* param : ('a * 'b) list        *****)
+(* Sortie : 'b                    ****)
+let minimumA l = let (a,b) = minimumArec l in b;; 
+
+(*************************************)
+let rec insererA
+(*************************************)
+(n,a)   (**** le couple **************)
+l       (**** la liste  **************)
+(*************************************)
+= match l with
+[] -> []
+  | (v,e)::r -> if a==e && v<=n (* si l'élément est présent et <=*)
+	        then (v,e)::insererA (n,a) r
+                else if a==e && n<v
+                      then (n,a)::l;;
+					  
+
+let rec contient (n,t) = function
+  | [] -> false
+  | (a,v)::q when v = t -> true
+  | _::q -> contient (n,t) q;;
+
+let elimine_doublon l = 
+  let rec aux acc = function
+    | [] -> List.rev acc
+    | t::q when contient t acc -> aux acc q
+    | t::q -> aux (t::acc) q in
+  aux [] l;;
+  
+(** Insérer un couple dans une liste ordonnée*)  
+let rec insererCouple
+n
+l
+ = match l with
+	[] -> [n]
+	| e::r -> if  n<e
+					then n::l
+					else e::insererCouple n r;;
+										       
+let insererA n l = elimine_doublon (insererCouple n l);;
+
+
